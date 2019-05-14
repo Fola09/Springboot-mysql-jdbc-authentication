75985bc24192a1c19963d5dac176d20c52f38497
Fola09 <folaolayiwola.fo@gmail.com>
2019-05-14 12:55:15 +0100

    Initial commit and dependencies configurations

---
+++ src/main/java/com/fola/springboot/springsecurity/mysql/jdbcauthentication/springbootspringsecuritymysqljdbcauthentication/SpringbootSpringsecurityMysqlJdbcAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.fola.springboot.springsecurity.mysql.jdbcauthentication.springbootspringsecuritymysqljdbcauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringsecurityMysqlJdbcAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringsecurityMysqlJdbcAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fola/springboot/springsecurity/mysql/jdbcauthentication/springbootspringsecuritymysqljdbcauthentication/SpringbootSpringsecurityMysqlJdbcAuthenticationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fola.springboot.springsecurity.mysql.jdbcauthentication.springbootspringsecuritymysqljdbcauthentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootSpringsecurityMysqlJdbcAuthenticationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
